4a6803e5aa6552828c1e2917f467ba3405d695f4
r3card0 <rsaldivar@1MXLRODRIGUE2.ad.here.com>
2025-11-12 11:58:31 -0600

    feat: add pyproject.toml file
    Change to an installable package

---
+++ src/python_oracle_toolkit/__init__.py
@@ -0,0 +1,4 @@
+from .get_connection_params import Params
+from .process_sql_query import SqlQuery
+
+__all__ = ['Params','SqlQuery']
---
+++ src/python_oracle_toolkit/get_connection_params.py
@@ -0,0 +1,30 @@
+from path_converter import PathConverter
+
+import json
+
+class Params:
+    def __init__(self,json_file:str):
+        self.filepath = json_file
+        self.connection_file = PathConverter(self.filepath).to_wsl()
+
+    # Class Method: Reads json file and returns a dictionary with connection parameters
+    def read_connection_params(self):
+        try:
+            with open(self.connection_file,'r') as config_file:
+                config = json.load(config_file)
+                # return params as diccionary
+                return config.get('oracle',{})
+        except FileNotFoundError as e:
+            print(f"Error: File not found")
+            return {e}
+        
+    # Establish connection
+    def connect(self):
+        # load connection params
+        config = self.read_connection_params()
+        user = config.get("user")
+        passw = config.get("password")
+        connection_chain = config.get("connection")
+        # Arrange parameters on a tuple
+        params = [user,passw,connection_chain]
+        return params
